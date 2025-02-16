9bc780f70a0f89f43cfae59979115567a87c9484
Magdasal John Mark P. <magdasaljohnmark29@gmail.com>
2025-02-16 18:39:07 +0800

    commit 2
    added "{ }" to the array

--- Exercise1.php
+++ Exercise1.php
@@ -30,9 +30,9 @@
                 echo "<h1> PHP Exercise 1 </h1>";
 
                 echo "<h4>\"We've seen all kinds of weather this month. 
-                At the beginning of the month, we had $weather[5] and $weather[6]. 
-                Then came $weather[1] with a few $weather[2] and some $weather[0]. 
-                At least we didn't get any $weather[3] or $weather[4].\"</h4>"
+                At the beginning of the month, we had {$weather[5]} and {$weather[6]}. 
+                Then came {$weather[1]} with a few {$weather[2]} and some {$weather[0]}. 
+                At least we didn't get any {$weather[3]} or {$weather[4]}.\"</h4>"
                     ?>
             </div>
 
